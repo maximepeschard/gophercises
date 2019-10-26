e9ae565c4206187f1e6bdb46285a8652ff011204
Maxime Peschard <maxime.peschard@gmail.com>
2019-10-27 00:00:42 +0200

    Cleanup exercise 3

--- 03_cyoa/story/story.go
+++ 03_cyoa/story/story.go
@@ -2,26 +2,31 @@ package story
 
 import "encoding/json"
 
+// An ArcOption represents a choice of story arc
 type ArcOption struct {
 	Text      string
 	TargetArc string `json:"arc"`
 }
 
+// An Arc represents a portion of a story
 type Arc struct {
 	Title   string
-	Story   []string
+	Text    []string `json:"story"`
 	Options []ArcOption
 }
 
+// Story represents a whole story with its arcs
 type Story struct {
 	Name string
 	Arcs map[string]Arc
 }
 
+// NewStory returns a new story (with no arcs) given a name
 func NewStory(name string) *Story {
 	return &Story{Name: name}
 }
 
+// ParseJSON uses the provided JSON data to populate the arcs of the story
 func (s *Story) ParseJSON(data []byte) (*Story, error) {
 	var arcs map[string]Arc
 	err := json.Unmarshal(data, &arcs)
